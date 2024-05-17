718add696b6795aca2a74ae072052fe7aa18c43e
Animace <eloquent.javascript@proton.me>
2024-05-17 17:00:34 +0300

    progress

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,11 +5,45 @@ import asyncHandler from 'express-async-handler'
 // @access Public 
 
 const authUser = asyncHandler (async (req, res) => {
-    res.status(401);
-    throw new Error('Something went wrong');
     res.status(200).json({message:'Auth User'});
 });
 
+//@desc  REgister a new user
+//route  POST /api/users
+// @access Public 
+
+const registerUser = asyncHandler (async (req, res) => {
+    res.status(200).json({message:'Register User'});
+});
+
+//@desc  logout user
+//route  POST /api/users/logout
+// @access Public 
+
+const logoutUser = asyncHandler (async (req, res) => {
+    res.status(200).json({message:'Logout User'});
+});
+
+//@desc  get user profile
+//route  GET /api/users/profile
+// @access Private 
+
+const getUserProfile = asyncHandler (async (req, res) => {
+    res.status(200).json({message:'User Profile'});
+});
+
+//@desc  update User Profile
+//route  PUT /api/users/profile
+// @access Public 
+
+const updateUserProfile = asyncHandler (async (req, res) => {
+    res.status(200).json({message:'Update user profile'});
+});
+
 export{
-    authUser
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile,
 };
