41914fddcedc131503c5ca9eae1cf454b5d866cb
Animace <eloquent.javascript@proton.me>
2024-05-20 17:05:47 +0300

    reg&Login setup

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -1,73 +1,81 @@
-import { useState, useEffect} from 'react';
-import {Link, useNavigate} from 'react-router-dom';
-import { Form, Button, Row, Col} from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { Form, Button, Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
-import {useLoginMutation} from '../slices/userApiSlice';
+import { useLoginMutation } from '../slices/usersApiSlice';
 import FormContainer from '../components/FormContainer';
 import { setCredentials } from '../slices/authSlice';
-import React from 'react'
+import { toast } from 'react-toastify';
+import Loader from '../components/Loader';
 
 const LoginScreen = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
+  const [login, { isLoading }] = useLoginMutation();
 
-    const[login,{ isLoading }] = useLoginMutation();
+  const { userInfo } = useSelector((state) => state.auth);
 
-    const { userInfo } = useSelector((state)=> state.auth);
-
-    useEffect(()=>{
-        if (userInfo) {
-            navigate('/');
-        }
-        },[navigate, userInfo]);
-    
-
-    const submitHandler = async (e) =>{
-        e.preventDefault();
-        console.log('submit');
+  useEffect(() => {
+    if (userInfo) {
+      navigate('/');
     }
-      return (
+  }, [navigate, userInfo]);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await login({ email, password }).unwrap();
+      dispatch(setCredentials({ ...res }));
+      navigate('/');
+    } catch (err) {
+      console.error('Login error:', err);
+      toast.error(err.data?.message || err.error || 'Login failed');
+    }
+  };
+
+  return (
     <FormContainer>
-        <h1>Sign In</h1>
+      <h1>Sign In</h1>
 
-        <Form onSubmit={submitHandler}>
-            <Form.Group className='my-2' controlId='email'>
-                <Form.Label>Email Address</Form.Label>
-                <Form.Control
-                  type='email'
-                  placeholder='Enter Email'
-                  value={email}
-                  onChange={(e)=> setEmail(e.target.value)}
-                ></Form.Control>
-            </Form.Group>
+      <Form onSubmit={submitHandler}>
+        <Form.Group className='my-2' controlId='email'>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Enter Email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
 
-            <Form.Group className='my-2' controlId='password'>
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type='password'
-                  placeholder='Enter Password'
-                  value={password}
-                  onChange={(e)=> setPassword(e.target.value)}
-                ></Form.Control>
-            </Form.Group>
+        <Form.Group className='my-2' controlId='password'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Enter Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
 
-            <Button type='submit' variant='primary' className='mt-3'>
-                Sign In
-            </Button>
+        {isLoading && <Loader />}
 
-            <Row className='py-3'>
-                <Col>
-                 New Customer? <Link to='/register'>Register</Link>
-                </Col>
-            </Row>
+        <Button type='submit' variant='primary' className='mt-3'>
+          Sign In
+        </Button>
 
-        </Form>
+        <Row className='py-3'>
+          <Col>
+            New Customer? <Link to='/register'>Register</Link>
+          </Col>
+        </Row>
+      </Form>
     </FormContainer>
-    )
-}
+  );
+};
 
 export default LoginScreen;
