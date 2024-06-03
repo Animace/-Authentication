69fb44e6f63cec28f7b9e9c6e87d77ab5fe34223
Animace <eloquent.javascript@proton.me>
2024-06-04 01:34:43 +0300

    Review

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -28,7 +28,7 @@ const Header = () => {
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand>MERN Authentication</Navbar.Brand>
+            <Navbar.Brand>MERN Authentication System</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
